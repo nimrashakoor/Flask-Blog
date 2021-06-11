46691e2ba4695f0b8f60ac1514d5b550f391a9b3
nimrashakoor <nimrashakoor@outlook.com>
2021-06-11 00:03:29 -0400

    finished projects page

--- app/__init__.py
+++ app/__init__.py
@@ -14,20 +14,20 @@ def projects():
     projects = []
     projects.append({'name':'Tracking the Population Growth of Baobab Trees', 'date':'00/00/0000', 'descrip':
     ['Baobab trees are endangered, partially due to drought, and tracking their growth is vital as many animals depend on them to survive',
-    'All data utilized is primary'], 'img':['/static/img/baobab_tree.jpg', 'Baobab Tree']})
+    'All data utilized is primary'], 'img':['/static/img/baobab_tree.jpg', 'Baobab Tree'], 'link':'https://github.com/JocelynHeckenkamp/Flask-Blog'})
     projects.append({'name':'Mobile App for Navigating the Safari', 'date':'00/00/0000',
     'descrip':['Utilizes a shortest-path algorithm for leaving the jungle', 'Has an option for avoiding waterholes'],
-    'img':['/static/img/travelling.jpg', 'Travelling through the Safari']})
+    'img':['/static/img/travelling.jpg', 'Travelling through the Safari'], 'link':'https://github.com/JocelynHeckenkamp/Flask-Blog'})
     projects.append({'name':'Finding Ancestry of Safari Animals', 'date':'00/00/0000',
     'descrip':['Uses DNA data to trace the history of safari animals as we know them today',
     'Support for other biomes is currently in the works'],
-    'img':['/static/img/rhinos.jpg', 'Travelling through the Safari']})
+    'img':['/static/img/rhinos.jpg', 'Travelling through the Safari'], 'link':'https://github.com/JocelynHeckenkamp/Flask-Blog'})
     return render_template('projects.html', projs=projects, url=os.getenv("URL"))
 
-@app.route('/blog/')
+@app.route('/blog')
 def blog():
     return render_template('blog.html', title="Blog")
 
-@app.route('/contact/')
+@app.route('/contact')
 def contact():
     return render_template('contact.html', title="Contact")
