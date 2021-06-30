8f438cc358438dd1a46bb126b69ef9484096d5ed
Cloud User <centos@ip-172-31-10-205.us-east-2.compute.internal>
2021-06-30 23:17:09 +0000

    adding dockerfile

--- app/__init__.py
+++ app/__init__.py
@@ -76,16 +76,12 @@ def register():
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif db.execute('SELECT id FROM user WHERE username = ?', (username,) ).fetchone() is not None:
             error = f"User {username} is already registered."
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
+            db.execute('INSERT INTO user (username, password) VALUES (?, ?)', 
+            (username, generate_password_hash(password)))
             db.commit()
             return f"User {username} created successfully"
         else:
@@ -101,9 +97,7 @@ def login():
         password = request.form.get('password')
         db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+        user = db.execute('SELECT * FROM user WHERE username = ?', (username,)).fetchone()
 
         if user is None:
             error = 'Incorrect username.'
